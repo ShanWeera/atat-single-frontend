65bfe97f30c6a17100647d61a4d4b646e0b10247
Shan <stwm2@student.london.ac.uk>
2021-04-09 21:53:39 +0800

    Fixed routes

--- src/api/Base.js
+++ src/api/Base.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const client = axios.create({
-  baseURL: 'https://atat-backend-devel.bioinfo.perdanauniversity.edu.my',
+  baseURL: 'https://atat-backend-devel.bioinfo.perdanauniversity.edu.my'
 });
 
 export const { get, post } = client;
